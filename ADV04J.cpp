1fdfe3ad659dd13e71be5684b2d09125511eb23a
Sarthak Sahu <vishalsahu5@gmail.com>
2018-04-02 23:58:59 +0530

    spoj

---
+++ ADV04J.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n){
+	if(n == 5)
+		return 4;
+	if(n == 3 || n == 4)
+		return 3;
+	int x = ceil((float)n/2.0);
+	return 1 + solve(x);
+}
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		cout << solve(n) << endl;
+	}
+	return 0;
+}
